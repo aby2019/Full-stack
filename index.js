cad20c39889eb13cbad104b6fbaf059a3bf056c6
aby2019 <Abhinav.gup@students.iiit.ac.in>
2020-06-16 15:06:27 +0530

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express');//common js modules
+// import express from 'express'; es 2015 module
+const app = express();
+
+app.get('/',(req,res)=>{
+  res.send({hi: 'bro'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
