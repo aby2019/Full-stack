817f8cb246bb7586ab843e29b766bc9c3865885d
aby2019 <Abhinav.gup@students.iiit.ac.in>
2020-06-28 03:21:10 +0530

    finished auth

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+//dev js    dont commit
+module.exports = {
+googleClientID: '16086516887-v4nvh076g8ujeafu5dmugou31on7ml09.apps.googleusercontent.com',
+googleClientSecret: 'RCabTtpSK7pRpJYOMxDc9H1j',
+mongoURI: 'mongodb+srv://aby_coder:kOR66XAb9zDXE5kl@cluster0.ijg5a.mongodb.net/naruto?retryWrites=true&w=majority',
+cookieKey: 'asdfghjkjhgfghj'
+};
