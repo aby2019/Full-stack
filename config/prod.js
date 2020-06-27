e7786e44ea15571e42a88e8b2d8c19656ae266ec
aby2019 <Abhinav.gup@students.iiit.ac.in>
2020-06-28 03:36:41 +0530

    finished auth

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
-//production keys
+// prod.js - production keys here!!
 module.exports = {
-googleClientID: process.env.GOOGLE_CLIEND_ID,
-googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-mongoURI: process.env.MONGO_URI,
-cookieKey: process.env.COOKIE_KEY
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
