f707c6784248228bbd69aea68beff177836c0c53
aby2019 <Abhinav.gup@students.iiit.ac.in>
2020-07-04 01:37:26 +0530

    survey done

--- config/dev.js
+++ config/dev.js
@@ -6,6 +6,7 @@ mongoURI: 'mongodb+srv://aby_coder:kOR66XAb9zDXE5kl@cluster0.ijg5a.mongodb.net/n
 cookieKey: 'asdfghjkjhgfghj',
 stripePublishableKey: 'pk_test_51H0FzBItfqruK5kgiEYLQsfT9RKF8o5LqsEdZ1vJTNC3s3hD2rP1pOtx2MOPaDG449RxpJhVNLXJFJxoT3x2m7dS00tNWhDTT6',
 stripeSecretKey: 'sk_test_51H0FzBItfqruK5kgIPigoI2Kq0hPjMgONZhfZdp0WUEnewjA3XT75RxMLqIdF0uZeHPZENSWbNATgj7Nh785UvIi00IKXKYujq',
-sendGridKey: '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'
+sendGridKey: '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',
+redirectDomain: 'http://localhost:3000'
 
 };
