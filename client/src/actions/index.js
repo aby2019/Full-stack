2898456ea662f1f6fd7995a5df9e18458df921b7
aby2019 <Abhinav.gup@students.iiit.ac.in>
2020-07-05 07:09:05 +0530

    complete

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
-
+import { FETCH_USER,FETCH_SURVEYS} from "./types";
 export const fetchUser = () => //action creater is called
     async dispatch => { //redux thunk will that function with dispatcher
     const res = await axios.get("/api/current_user");
@@ -21,6 +20,10 @@ export const submitSurvey = (values,history) => async dispatch => {
   const res=await axios.post('/api/surveys',values);
   history.push('/surveys');
   dispatch({type: FETCH_USER,payload: res.data});
+};
 
 
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+  dispatch({type: FETCH_SURVEYS,payload:res.data})
 };
